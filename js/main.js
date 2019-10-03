bcda1b031f60752cc6c9a77696f7ce8d4a78bf00
XXIlativXX <55597983+XXIlativXX@users.noreply.github.com>
2019-10-03 19:25:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-alert(window.location.href)
+if(window.location.href != "https://xxilativxx.github.io/m") {
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/Blackberry/i) || navigator.userAgent.match(/WebOs/i)) { // detect mobile browser
+        window.location.replace("m"); // redirect if mobile browser detected
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function(){
 
